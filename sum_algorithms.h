af5fc5968499d710276b660cae4144aca074dbe4
Haris0059 <haris.skeledzija@stu.ibu.edu.ba>
2025-10-30 12:54:16 +0100

    Assignment 4 Finished

---
+++ sum_algorithms.h
@@ -0,0 +1,16 @@
+//
+// Created by haris on 10/30/25.
+//
+#ifndef SUM_ALGORITHMS_H
+#define SUM_ALGORITHMS_H
+
+/*
+ * Standard double precision sum
+ */
+double do_sum(double* restrict var, long ncells);
+double do_ldsum(double* var, long ncells);
+double do_pair_sum(double* var, long ncells);
+double do_kahn_sum(double* var, long ncells);
+double do_knuth_sum(double* var, long ncells);
+
+#endif //IT_2004_SUM_ALGORITHMS_H
