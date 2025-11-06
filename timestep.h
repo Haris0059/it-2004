1b24f083d4ec2f7fa7e3dd9e8452b4842207b52c
Haris0059 <haris.skeledzija@stu.ibu.edu.ba>
2025-11-06 09:45:23 +0100

    .

--- timestep.h
+++ timestep.h
@@ -1,11 +1,7 @@
-//
-// Created by hajro on 5. 11. 2025..
-//
-
-#ifndef PARALLELCOMPUTING_TIMESTEP_H
-#define PARALLELCOMPUTING_TIMESTEP_H
+#ifndef IT_2004_TIMESTEP_H
+#define IT_2004_TIMESTEP_H
 #define REAL_CELL 1
 
 double timestep(int ncells, double g, double sigma, int* restrict celltype,
                 double *H, double *U, double *V, double *dx, double *dy);
-#endif //PARALLELCOMPUTING_TIMESTEP_H
+#endif //IT_2004_TIMESTEP_H
