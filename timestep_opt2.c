bb5906f8e84b135a9c1a6ce9971320fed25f4846
Haris0059 <haris.skeledzija@stu.ibu.edu.ba>
2025-11-06 09:43:40 +0100

    Implemented timestep.c and timestep_opt from 1 to 3

--- timestep_opt2.c
+++ timestep_opt2.c
@@ -2,10 +2,11 @@
 #include "timestep.h"
 #define REAL_CELL 1
 
-double timestep(int ncells, double g, double sigma, int* restrict celltype,
+double timestep(int ncells, double g, double sigma, int* celltype,
                 double* H, double* U, double* V, double* dx, double* dy){
    double wavespeed, xspeed, yspeed, dt;
    double mymindt = 1.0e20;
+#pragma omp simd private(wavespeed, xspeed, yspeed, dt) reduction(min:mymindt)
    for (int ic=0; ic<ncells ; ic++) {
       if (celltype[ic] == REAL_CELL) {
          wavespeed = sqrt(g*H[ic]);
--- timestep_opt3.c
+++ timestep_opt3.c
@@ -2,16 +2,16 @@
 #include "timestep.h"
 #define REAL_CELL 1
 
-double timestep(int ncells, double g, double sigma, int* restrict celltype,
+double timestep(int ncells, double g, double sigma, int* celltype,
                 double* H, double* U, double* V, double* dx, double* dy){
-   double wavespeed, xspeed, yspeed, dt;
    double mymindt = 1.0e20;
+#pragma omp simd reduction(min:mymindt)
    for (int ic=0; ic<ncells ; ic++) {
       if (celltype[ic] == REAL_CELL) {
-         wavespeed = sqrt(g*H[ic]);
-         xspeed = (fabs(U[ic])+wavespeed)/dx[ic];
-         yspeed = (fabs(V[ic])+wavespeed)/dy[ic];
-         dt=sigma/(xspeed+yspeed);
+         double wavespeed = sqrt(g*H[ic]);
+         double xspeed = (fabs(U[ic])+wavespeed)/dx[ic];
+         double yspeed = (fabs(V[ic])+wavespeed)/dy[ic];
+         double dt=sigma/(xspeed+yspeed);
          if (dt < mymindt) mymindt = dt;
       }
    }
